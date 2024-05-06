3732774736128574151056b1aa02728225f4658f
iurii presnov <presnov3w@gmail.com>
2024-05-06 14:04:22 +0300

    sprint 7 java automation yandex

---
+++ src/main/java/qa/scooter/models/courier/CourierSteps.java
@@ -0,0 +1,53 @@
+package qa.scooter.models.courier;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static qa.scooter.constants.RestApiConstants.*;
+
+public class CourierSteps {
+
+    public static RequestSpecification requestSpec() {
+        return given()
+                .log()
+                .all()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URL);
+    }
+
+    public ValidatableResponse createCourier(CourierInfo courierInfo) {
+        return requestSpec()
+                .body(courierInfo)
+                .when()
+                .post(POST_COURIER_CREATE)
+                .then()
+                .log()
+                .all();
+    }
+
+    public ValidatableResponse courierAuthorization(CourierLoginCredentials courierInfo) {
+        return requestSpec()
+                .body(courierInfo)
+                .when()
+                .post(POST_COURIER_LOGIN)
+                .then()
+                .log()
+                .all();
+    }
+
+
+    public ValidatableResponse deleteCourier(int courierId) {
+        return requestSpec()
+                .when()
+                .delete(DELETE_COURIER + courierId)
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body("ok", equalTo(true));
+    }
+
+}
